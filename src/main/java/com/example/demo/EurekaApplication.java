6559cd235fb5cda5e52d860d521e0a77f3753d2d
Saiguer10 <eljosemsp@gmail.com>
2025-05-21 16:51:32 +0200

    "Commit con solucion de lab corriendo"

--- src/main/java/com/example/demo/EurekaApplication.java
+++ src/main/java/com/example/demo/EurekaApplication.java
@@ -3,11 +3,9 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaServer
-@EnableFeignClients
 public class EurekaApplication {
 
 	public static void main(String[] args) {
